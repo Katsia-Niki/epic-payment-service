3c04749b1e4b5a22fd7528310e3c905b5c1a5cfa
Katsia-Niki <katsiaryna.niki@gmail.com>
2026-08-21 15:01:31 +0300

    initial commit

---
+++ src/main/java/by/nikiforova/payment/EpicPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package by.nikiforova.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicPaymentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/nikiforova/payment/EpicPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.nikiforova.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
